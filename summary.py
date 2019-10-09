005f692055a4517610fa509655581130bd4720d2
morika <sm+github@morikaglobal.com>
2019-10-09 16:26:54 +0700

    fixing deploy issue

--- summary.py
+++ summary.py
@@ -63,7 +63,6 @@ def search_results(urlsearch):
 
     return render_template("results.html", search_string = search_string, data = data, title=title, published_date=published_date, author = author, image = image, cloud = cloud)
 
-    # return render_template("results.html", search_string = search_string, data = data, title=title, published_date=published_date, author = author, image = image, cloud = cloud, summary = summary)
-            
+    # return render_template("results.html", search_string = search_string, data = data, title=title, published_date=published_date, author = author, image = image, cloud = cloud, summary = summary)           
 if __name__ == '__main__':
       app.run()
