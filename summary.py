0c8bd66ecdbbb77b50cf66f106cca701c4aa9ed4
morika <sm+github@morikaglobal.com>
2019-10-09 16:17:56 +0700

    fixing deploy issue

--- summary.py
+++ summary.py
@@ -57,10 +57,10 @@ def search_results(urlsearch):
 
     image = article.top_image
 
-    # cloud = get_wordcloud(data)
+    cloud = get_wordcloud(data)
 
-    # summary = article.summary
-    return render_template("results.html", search_string = search_string, data = data, title=title, published_date=published_date, author = author, image = image)
+    summary = article.summary
+    return render_template("results.html", search_string = search_string, data = data, title=title, published_date=published_date, author = author, image = image, cloud = cloud, summary = summary)
 
     # return render_template("results.html", search_string = search_string, data = data, title=title, published_date=published_date, author = author, image = image, cloud = cloud, summary = summary)
             
