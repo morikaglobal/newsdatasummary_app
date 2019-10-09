1158ff60771cba8573fdafeaa7bb12ac2f62e474
morika <sm+github@morikaglobal.com>
2019-10-09 18:10:32 +0700

    fixed errors

--- summary.py
+++ summary.py
@@ -60,9 +60,9 @@ def search_results(urlsearch):
 
     cloud = get_wordcloud(data)
 
-    summary = data
+    # summary = data
 
-    return render_template("results.html", search_string = search_string, data = data, title=title, published_date=published_date, author = author, image = image, cloud = cloud, summary = summary)
+    return render_template("results.html", search_string = search_string, data = data, title=title, published_date=published_date, author = author, image = image, cloud = cloud)
 
     # return render_template("results.html", search_string = search_string, data = data, title=title, published_date=published_date, author = author, image = image, cloud = cloud, summary = summary)           
 if __name__ == '__main__':
