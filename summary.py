0a24fcc96ad40395dd615f75b61dcbac6cb3b7f6
morika <sm+github@morikaglobal.com>
2023-04-23 10:07:40 +0900

    disabled wordcloud

--- summary.py
+++ summary.py
@@ -7,19 +7,19 @@ from forms import UrlSearchForm
 # nltk.data.path.append('./nltk_data/')
 
 from newspaper import Article
-from wordcloud import WordCloud
+# from wordcloud import WordCloud
 
 import base64
 import io
 import datetime
 
-def get_wordcloud(text):
-    pil_img = WordCloud().generate(text=text).to_image()
-    img = io.BytesIO()
-    pil_img.save(img, "PNG")
-    img.seek(0)
-    img_b64 = base64.b64encode(img.getvalue()).decode()
-    return img_b64
+# def get_wordcloud(text):
+#     pil_img = WordCloud().generate(text=text).to_image()
+#     img = io.BytesIO()
+#     pil_img.save(img, "PNG")
+#     img.seek(0)
+#     img_b64 = base64.b64encode(img.getvalue()).decode()
+#     return img_b64
 
 app = Flask(__name__)
 
